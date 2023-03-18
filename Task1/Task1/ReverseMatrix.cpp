edda9cc5f8df6d8a37f4ff20b7866f1210b8a004
Alexey Polozhencev <90678468+Alex07062002@users.noreply.github.com>
2023-03-18 09:28:28 +0300

    Update ReverseMatrix.cpp

--- Task1/Task1/ReverseMatrix.cpp
+++ Task1/Task1/ReverseMatrix.cpp
@@ -3,12 +3,13 @@
 int main()
 {
     ReverseMatrix reverse_matrix;
-    int rows,columns;
+    int *rows = new int;
+    int *columns = new int;
     std::cout<<"Enter size of matrix:\n";
-    std::cin>>rows>>columns;
-    int** matrix = reverse_matrix.inputFromUser(rows,columns);
-    reverse_matrix.reverseMatrix(matrix,rows,columns);
-    reverse_matrix.output(matrix,rows,columns);
+    std::cin>>*rows>>*columns;
+    int** matrix = reverse_matrix.inputFromUser(*rows,*columns);
+    reverse_matrix.reverseMatrix(matrix,*rows,*columns);
+    reverse_matrix.output(matrix,*rows,*columns);
     return 0;
 }
 
